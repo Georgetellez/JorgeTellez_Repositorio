cf57e1b772609527ca7b9e6bee45112974d24d9c
Georgetellez <40368412+Georgetellez@users.noreply.github.com>
2018-06-19 13:39:48 -0500

    Ejercicio 1

---
+++ e1.py
@@ -0,0 +1,29 @@
+edad1=20
+
+if(edad1>18):
+
+	print("Eres mayor  puedes pasar")
+
+elif(edad1==18):
+
+	print("Tienes la edad requerida, presenta INE")
+
+elif(edad1 != 18):
+
+	print("Eres mayor o menor de edad")
+
+else:
+	print("Eres menor de edad")
+#else: 
+#pass donde pass sirve para que no te de error en caso de que en un futuro agreugres algo a ese caso
+
+a=15
+b=3
+n=0
+n=a
+a=b
+b=n
+print(a)
+print(b)
+
+
