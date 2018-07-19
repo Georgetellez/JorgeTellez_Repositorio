f308bdff9ee18e67d789190f3e6860e42ed17610
Georgetellez <40368412+Georgetellez@users.noreply.github.com>
2018-07-19 15:43:52 -0500

    Delete modulos.py

--- pf/modulos.py
+++
@@ -1,8 +0,0 @@
-def imprimirarchivo(cadena):
-	archivousuario = open("archivousuario.txt","a")
-	archivousuario.write("\n"+cadena)
-	archivousuario.close()
-def imprimirticket(productos):
-	archivousuario = open("Ticketcompras.txt","a")
-	archivousuario.write("\n"+productos)
-	archivousuario.close()
